c9e70b7915bc5feb82bbe51b87c98f042be65e13
Alison Zhang <alisonzthu@gmail.com>
2017-12-25 21:58:11 +0800

    added AuthenticationDialog and AuthenticationListener

--- app/src/main/java/com/example/macstudio/instagramalison/listener/AuthenticationListener.java
+++ app/src/main/java/com/example/macstudio/instagramalison/listener/AuthenticationListener.java
@@ -4,5 +4,6 @@ package com.example.macstudio.instagramalison.listener;
  * Created by macstudio on 12/18/17.
  */
 
-public class AuthenticationListener {
+public interface AuthenticationListener {
+    void onCodeReceived(String code);
 }
--- app/src/main/java/com/example/macstudio/instagramalison/ui/MainActivity.java
+++ app/src/main/java/com/example/macstudio/instagramalison/ui/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.macstudio.instagramalison.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.macstudio.instagramalison.R;
+import com.example.macstudio.instagramalison.dialog.AuthenticationDialog;
+import com.example.macstudio.instagramalison.listener.AuthenticationListener;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AuthenticationListener{
+    private AuthenticationDialog auth_dialog;
+//    private Button btn_get_access_token;
+    private String mAuthToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        btn_get_access_token = findViewById(R.id.btn_get_access_token);
+        auth_dialog = new AuthenticationDialog(MainActivity.this, this);
+        auth_dialog.show();
+    }
+
+    @Override
+    public void onCodeReceived(String code) {
+        if (code != null) {
+            auth_dialog.dismiss();
+        }
     }
 }
