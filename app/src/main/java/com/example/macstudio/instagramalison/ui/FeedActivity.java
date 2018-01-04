4fa7174d710a52d97e0ba93897a28fe9597d2a57
Alison Zhang <alisonzthu@gmail.com>
2018-01-04 12:13:22 +0800

    added log out button on both activities. should delete the MainActivity one after testing

--- app/src/main/java/com/example/macstudio/instagramalison/ui/FeedActivity.java
+++ app/src/main/java/com/example/macstudio/instagramalison/ui/FeedActivity.java
@@ -3,6 +3,8 @@ package com.example.macstudio.instagramalison.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -43,6 +45,7 @@ public class FeedActivity extends AppCompatActivity {
     public void fetchData() {
         Call<InstagramResponse> call = ServiceGenerator.createGetFeedService().getOwnPhotos(access_token);
         call.enqueue(new Callback<InstagramResponse>() {
+
             @Override
             public void onResponse(Call<InstagramResponse> call, Response<InstagramResponse> response) {
                 if (response.body() != null) {
@@ -62,5 +65,27 @@ public class FeedActivity extends AppCompatActivity {
         // when there's no data??
 
         // do I need to call clearListView() anywhere?
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                //your action
+                Toast.makeText(FeedActivity.this, "action_settings", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/macstudio/instagramalison/ui/MainActivity.java
+++ app/src/main/java/com/example/macstudio/instagramalison/ui/MainActivity.java
@@ -1,35 +1,38 @@
 package com.example.macstudio.instagramalison.ui;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.macstudio.instagramalison.R;
-import com.example.macstudio.instagramalison.api.ApplicationConsts;
-import com.example.macstudio.instagramalison.api.model.TokenResponse;
-import com.example.macstudio.instagramalison.api.services.ServiceGenerator;
 import com.example.macstudio.instagramalison.dialog.AuthenticationDialog;
 import com.example.macstudio.instagramalison.listener.AuthenticationListener;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements AuthenticationListener{
     private AuthenticationDialog auth_dialog;
     private Button btn_connect;
-    private TextView failureText;
-    private String authToken;
 
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+//        Window window = this.getWindow();
+//        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
 
         btn_connect = findViewById(R.id.btn_connect);
 
@@ -59,5 +62,27 @@ public class MainActivity extends AppCompatActivity implements AuthenticationLis
     @Override
     public void onError(String error) {
         Toast.makeText(MainActivity.this, "Error message: " + error, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                //your action
+                Toast.makeText(MainActivity.this, "action_settings", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
