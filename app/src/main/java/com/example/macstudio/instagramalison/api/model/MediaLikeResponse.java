417154d68f5f776616dd38661186d93185e301f6
Alison Zhang <alisonzthu@gmail.com>
2018-01-07 23:33:04 -0800

    created likeservice for get/post/delete likes to a photo

---
+++ app/src/main/java/com/example/macstudio/instagramalison/api/model/MediaLikeResponse.java
@@ -0,0 +1,8 @@
+package com.example.macstudio.instagramalison.api.model;
+
+/**
+ * Created by macstudio on 1/8/18.
+ */
+
+public class MediaLikeResponse {
+}
---
+++ app/src/main/java/com/example/macstudio/instagramalison/api/services/LikeService.java
@@ -0,0 +1,27 @@
+package com.example.macstudio.instagramalison.api.services;
+
+import com.example.macstudio.instagramalison.api.model.InstagramResponse;
+import com.example.macstudio.instagramalison.api.model.MediaLikeResponse;
+
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by macstudio on 1/8/18.
+ */
+
+public interface LikeService {
+    @GET("v1/media/{media-id}/likes")
+    Call<MediaLikeResponse> getMediaLikes(@Path("media-id") int mediaId,
+                                          @Query("access_token") String access_token);
+
+    @POST("v1/media/{media-id}/likes")
+    Call<MediaLikeResponse> postLikeMedia();
+
+    @DELETE("v1/media/{media-id}/likes")
+    Call<MediaLikeResponse> deleteLikeMedia();
+}
